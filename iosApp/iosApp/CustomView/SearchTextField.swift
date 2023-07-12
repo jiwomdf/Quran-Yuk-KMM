d9846670b37bee4e6bf7b6a3dbe61e3e8977c7c6
jiwomdf <mdf.jiwo@gmail.com>
2023-07-12 21:08:48 +0700

    finish initiate pod dep and search syrah

---
+++ iosApp/iosApp/CustomView/SearchTextField.swift
@@ -0,0 +1,51 @@
+//
+//  FormTextField.swift
+//  iosApp
+//
+//  Created by MAC on 12/07/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class SearchTextField: UITextField {
+
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    struct Constants {
+        static let sidePadding: CGFloat = 10
+        static let topPadding: CGFloat = 6
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(
+            x: bounds.origin.x + Constants.sidePadding,
+            y: bounds.origin.y + Constants.topPadding,
+            width: bounds.size.width - Constants.sidePadding * 2,
+            height: bounds.size.height - Constants.topPadding * 2
+        )
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return self.textRect(forBounds: bounds)
+    }
+
+}
--- iosApp/iosApp/ViewController/Main/MainQuranViewController.swift
+++ iosApp/iosApp/ViewController/Main/MainQuranViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import shared
+import MaterialComponents.MaterialSnackbar
 
 final class MainQuranViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -16,12 +17,18 @@ final class MainQuranViewController: UIViewController, UITableViewDataSource, UI
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBAction func seatchEditingChanged(_ sender: SearchTextField) {
+        viewModel.searchSurah(rawKeyWord: sender.text ?? "")
+    }
+    
+    
     private lazy var viewModel: MainQuranViewModel = {
         let remote = RemoteDataSourceImpl(quranApi: QuranApi())
         let local = DatabaseModule().noteDataSource
         let vm = MainQuranViewModel(quranRepository: QuranRepositoryImpl(remote: remote, local: local))
         vm.didSucceedGetAllSurah = reloadViews
         vm.didFailedGetAllSurah = showErrorMessage
+        vm.didSearchSurah = reloadViews
         return vm
     }()
     
@@ -33,13 +40,14 @@ final class MainQuranViewController: UIViewController, UITableViewDataSource, UI
     
     
     private func reloadViews(data: [Surah]?) {
-        print("data count: \(String(describing: data?.count))")
+        print("jiwo data count: \(String(describing: data?.count))")
         listSurah = data
         tableView.reloadData()
     }
     
     private func showErrorMessage(error: String) {
-        
+        let message = MDCSnackbarMessage(text: "Something went wrong")
+        MDCSnackbarManager.default.show(message)
     }
     
     
@@ -50,13 +58,10 @@ final class MainQuranViewController: UIViewController, UITableViewDataSource, UI
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constans.SURAH_CELL_ID, for: indexPath) as! SurahTableViewCell
-        
         let surah = self.listSurah?[indexPath.row]
-
         if(surah != nil) {
             cell.setCell(data: surah!)
         }
-        
         return cell
     }
     
