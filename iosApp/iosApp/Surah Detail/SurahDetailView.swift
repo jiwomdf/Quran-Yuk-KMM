344b4b916fec3b25cd79486686241339cdeeeea1
jiwomdf <mdf.jiwo@gmail.com>
2023-07-16 12:49:13 +0700

    update UI

---
+++ iosApp/iosApp/Surah Detail/SurahDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  SurahDetailView.swift
+//  iosApp
+//
+//  Created by MAC on 14/07/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SurahDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SurahDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SurahDetailView()
+    }
+}
