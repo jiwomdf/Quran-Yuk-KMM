9d0a4606c0a6f2302017cc0df1c87493b653d736
jiwomdf <mdf.jiwo@gmail.com>
2023-07-20 22:50:23 +0700

    enhance exception

--- shared/src/commonMain/kotlin/com/programmergabut/quranyuk/domain/repository/QuranRepository.kt
+++ shared/src/commonMain/kotlin/com/programmergabut/quranyuk/domain/repository/QuranRepository.kt
@@ -4,6 +4,8 @@ import com.programmergabut.quranyuk.domain.model.ReadSurah
 import com.programmergabut.quranyuk.domain.model.Surah
 
 interface QuranRepository {
+    @Throws(Exception::class)
     suspend fun getAllSurah(): List<Surah>
+    @Throws(Exception::class)
     suspend fun getReadSurah(surahId: Int): ReadSurah?
 }
